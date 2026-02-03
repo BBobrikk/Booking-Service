63f798808ba4f4a7247c1e2ff1767428665805cb
PC <aaa249148@gmail.com>
2026-02-03 17:52:36 +0300

    add constraints for tables

--- application/models/rooms.py
+++ application/models/rooms.py
@@ -13,14 +13,14 @@ class RoomsORM(Base):
     beds: Mapped[int]
     bookings: Mapped[list["BookingsORM"]] = relationship(back_populates="room")
 
-    # __table_args__ = (
-    #     CheckConstraint(
-    #         "grade in ('standard', 'luxe', 'president')", "CHK_grade_valid"
-    #     ),
-    #     CheckConstraint(
-    #         "number > 0 ", "CHK_number_valid"
-    #     ),
-    #     CheckConstraint(
-    #         "beds between 1 and 4", "CHK_beds_valid"
-    #              )
-    # )
+    __table_args__ = (
+        CheckConstraint(
+            "grade in ('standard', 'luxe', 'president')", "CHK_grade_valid"
+        ),
+        CheckConstraint(
+            "number > 0 ", "CHK_number_valid"
+        ),
+        CheckConstraint(
+            "beds between 1 and 4", "CHK_beds_valid"
+                 )
+    )
